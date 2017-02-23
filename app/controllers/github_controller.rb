b74f53a5530a0172c52f8f8c30fdf3e996afef20
Courtland Messam <messamcourtland@gmail.com>
2017-02-23 11:22:24 -0500

    fixed incorect variable bug

--- app/controllers/github_controller.rb
+++ app/controllers/github_controller.rb
@@ -1,5 +1,5 @@
 class GithubController < ApplicationController
   def index
-    @profile = HTTParty.get("https://api.github.com/users/#{username}").parsed_response
+    @profile = HTTParty.get("https://api.github.com/users/1").parsed_response
   end
 end
